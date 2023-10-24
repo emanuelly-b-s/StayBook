a7145fa7fde7a9100d7556976197c468924b2524
Emanuelly <Emanuellybs14@gmail.com>
2023-10-24 17:05:08 -0300

    booking schema table created

---
+++ back/models/Booking.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const Booking = mongoose.model('User', {
+
+    start_date: Date,
+    end_date: Date,
+    user: [
+        {
+            name: String,
+            phone_number: String,
+            cpf: String,
+            date_birth: Date,
+            email: String
+        }
+    ],
+    payment: [
+        {
+            booking_amount: Number,
+            form_payment: String,
+        }
+    ],
+    dependents: [
+        {
+            number_children: Number,
+            number_adults: Number
+        }
+    ]
+});
+
+module.exports = Booking;
