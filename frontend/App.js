7ad66afcba1e8f83a3d5f0cd19ae85350c37a924
nycollassobolevski <sobolevski.academy@gmail.com>
2023-10-26 17:25:35 -0300

    ADD: filter

--- frontend/App.js
+++ frontend/App.js
@@ -10,8 +10,7 @@ import RedefinePassword from './src/screens/RedefinePasswordPage';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
-
-library.add(fas);
+library.add(fas)  ;
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -23,11 +22,11 @@ export default function App() {
           headerStyle: { elevation: 0 },
           cardStyle: { backgroundColor: '#000' }
         }}>
+          <Stack.Screen name="home" options={{ headerShown: false }} component={HomePage} />
           <Stack.Screen name="cadastro" options={{ headerShown: false }} component={Cadastro} />
           <Stack.Screen name="recover" options={{ headerShown: false }} component={RecoverPassword} />
           <Stack.Screen name="redefine" options={{ headerShown: false }} component={RedefinePassword} />
           <Stack.Screen name="confirm" options={{ headerShown: false }} component={ConfirmCode} />
-          <Stack.Screen name="home" options={{ headerShown: false }} component={HomePage} />
           <Stack.Screen name="login" options={{ headerShown: false }} component={Login} />
         </Stack.Navigator>
       </UsersProvider>
--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,17 +1,22 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 import logo from "../../assets/StayBook Icon.png"
 
 export default function Nav ( params ) {
     return(
         <View style={styles.NavContainer}>
-            <Image source={{uri: "../../assets/StayBook Icon.png"}} />
+            <Image source={logo} style={styles.IconStayBook} />
+            <Pressable style={styles.BurgerButtom}>
+                <View style={styles.BurgerItem}></View>
+                <View style={styles.BurgerItem}></View>
+                <View style={styles.BurgerItem}></View>
+            </Pressable>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     NavContainer:{
-        position:'absolute',
+        position:'fixed',
         zIndex:2,
         top:0,
         backgroundColor:"#160D00",
@@ -20,5 +25,23 @@ const styles = StyleSheet.create({
         color:"#fff",
         alignItems:"center",
         justifyContent:"center"
+    },
+    IconStayBook: {
+        width:150,
+        height:50,
+        resizeMode:"contain"
+    },
+    BurgerItem:{
+        width:40,
+        height:4,
+        backgroundColor:"#D9D9D9"
+    },
+    BurgerButtom: {
+        flexDirection:'column',
+        alignItems:'center',
+        justifyContent:'center',
+        position:'fixed',
+        top:25,left:10,
+        gap:7
     }
 })
